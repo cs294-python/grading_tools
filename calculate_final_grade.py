3340a66cebb21af7f27150655c7cdc7a5bd60eab
Josh <shua387@yahoo.com>
2015-08-27 10:55:54 -1000

    Update calculate_final_grade.py
    Shortened the import (and relevant function calls). Corrected some grammar.

--- calculate_final_grade.py
+++ calculate_final_grade.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 
-import sys
+from sys import argv, stderr, exit
 
 # Check command line args
-if len(sys.argv) != 4:
-    sys.stderr.write("usage: python calculate_final_grade.py <assignment points> <blog points> <project points>\n")
-    sys.exit()
+if len(argv) != 4:
+    stderr.write("usage: python calculate_final_grade.py <assignment points> <blog points> <project points>\n")
+    exit()
 
 # Attempt to parse command line args
-assignment_points = int(sys.argv[1])
-blog_points = int(sys.argv[2])
-project_points = int(sys.argv[3])
+assignment_points = int(argv[1])
+blog_points = int(argv[2])
+project_points = int(argv[3])
 
 # Calculate total points and determine letter grade
 total_points = assignment_points + blog_points + project_points
 if total_points >= 90:
     print("You get an 'A'")
 elif total_points >= 80:
-    print("You get an 'B'")
+    print("You get a 'B'")
 elif total_points >= 70:
-    print("You get an 'C'")
+    print("You get a 'C'")
 elif total_points >= 60:
-    print("You get an 'D'")
+    print("You get a 'D'")
 else:
     print("You get an 'F'")
