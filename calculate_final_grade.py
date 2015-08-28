9085d7368b32b3050efdfe2f56efac07fc45fe46
Josh <joshuacw@users.noreply.github.com>
2015-08-28 13:40:41 -1000

    Handling data type exceptions
    BAM!
    Also, I learned about try-except blocks. I've wanted something like this in the past, but didn't previously know about this. Thanks!
    (I'll admit, I didn't read through the link when you initially sent it to me. That has often been one of my shortcomings - doing before reading. Need...to....over...come...)

--- calculate_final_grade.py
+++ calculate_final_grade.py
@@ -7,21 +7,25 @@ if len(argv) != 4:
     stderr.write("usage: python calculate_final_grade.py <assignment points> <blog points> <project points>\n")
     exit()
 
-# Attempt to parse command line args
-assignment_points = float(argv[1])
-blog_points = float(argv[2])
-project_points = float(argv[3])
+# Handling data type exception. Attempt to parse command line args
+try:
+    assignment_points = float(argv[1])
+    blog_points = float(argv[2])
+    project_points = float(argv[3])
+except:
+    print("Please type in numeric values for points. Try again, you goon.")
+    exit()
 
 # Check that points are within range.
 if assignment_points > 35:
     print("Please enter a value less than or equal to 35 for assignment points.")
 if blog_points > 25:
-        print("Please enter a value less than or equal to 25 for blog points.")
+    print("Please enter a value less than or equal to 25 for blog points.")
 if project_points > 50:
-        print("Please enter a value less than or equal to 50 for project points.")
+    print("Please enter a value less than or equal to 50 for project points.")
 
 # Calculate total points and determine letter grade
-total_points = assignment_points + blog_points + project_points
+total_points = assignment_points + blog_points + project_points # 35, 25, 50
 if (assignment_points <= 35) and (blog_points <= 25) and (project_points <= 50):
     if total_points >= 90:
         print("You get an 'A'")
